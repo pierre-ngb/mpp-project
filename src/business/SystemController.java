7eda7033c773b5a4e15ad4e9ed98480b13ae8f98
kashifjawad <kashifjawad@172.19.140.78>
2021-03-31 20:31:36 -0500

    changes

--- src/business/SystemController.java
+++ src/business/SystemController.java
@@ -15,14 +15,17 @@ public class SystemController implements ControllerInterface {
 	public void login(String id, String password) throws LoginException {
 		DataAccess da = new DataAccessFacade();
 		HashMap<String, User> map = da.readUserMap();
+		
 		if(!map.containsKey(id)) {
 			throw new LoginException("ID " + id + " not found");
 		}
+		
 		String passwordFound = map.get(id).getPassword();
 		if(!passwordFound.equals(password)) {
 			throw new LoginException("Password incorrect");
 		}
 		currentAuth = map.get(id).getAuthorization();
+		System.out.println("what");
 		
 	}
 	@Override
