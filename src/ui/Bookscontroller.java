0865820e01b2eee75a18221dd35ef7137d4e89af
kashifjawad <kashifjawad@172.19.140.78>
2021-03-31 20:33:41 -0500

    added files

---
+++ src/ui/Bookscontroller.java
@@ -0,0 +1,31 @@
+package ui;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+public class Bookscontroller {
+	
+	 private Button button1;
+	 
+	 public void butonClicked(ActionEvent event) {
+		 
+	 System.out.println("working");
+//	 Start.setRoot();
+
+	}
+
+
+}
+
+
+
+
+
+
+ 
+
+ 
+
